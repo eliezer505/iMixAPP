2074f0b5d03414893358ef91d5899510dcb84f78
arama <aramainfo@gmail.com>
2016-12-17 19:23:32 +0200

    Limit the ng-repeat pagination to 10 pages.

--- app/controllers/searchCtrl.js
+++ app/controllers/searchCtrl.js
@@ -1,40 +1,63 @@
 (function() {
-	var app= angular.module("app");
+	var app = angular.module("app");
 	app.controller("searchCtrl", searchCtrl);
 
 	//?apiKey=5178650f2fb747a1a4c48cf9b86bd154//
-	
-	function searchCtrl($scope,$http) {
-		var API_KEY="?apiKey=5178650f2fb747a1a4c48cf9b86bd154";
-		var URL="http://addb.absolutdrinks.com/drinks/";
-        var QUERY="";
-		$scope.searchDrinks="";
-		$scope.title="search ctrl";
-		$scope.getDrinks=function(){
-		var promise = $http.jsonp( "http://addb.absolutdrinks.com/quickSearch/drinks/"+ $scope.searchDrinks +"/?apiKey=5178650f2fb747a1a4c48cf9b86bd154&pageSize=48&callback=JSON_CALLBACK");
-            promise.success(function(data){
-                console.log("this is my data",data);
-                 console.log("this is my data",data.result.videos);
-                
-            });
-		;
-		promise.then(successCallback,failureCallback);
+
+	function searchCtrl($scope, $http) {
+		var API_KEY = "?apiKey=5178650f2fb747a1a4c48cf9b86bd154";
+		var URL = "http://addb.absolutdrinks.com/drinks/";
+		var QUERY = "";
+		$scope.searchDrinks = "";
+		$scope.nextp = 0
+		$scope.title = "search ctrl";
+		$scope.getDrinks = function(nextp) {
+			var promise = $http.jsonp("http://addb.absolutdrinks.com/quickSearch/drinks/" + $scope.searchDrinks + "/?apiKey=5178650f2fb747a1a4c48cf9b86bd154&start=" + nextp + "&pageSize=12&callback=JSON_CALLBACK");
+			promise.success(function(data) {
+				$scope.total = data.totalResult;
+				console.log("this is my data", data);
+				console.log("this is my data", data.result.videos);
+  
+			});
+			;
+			promise.then(successCallback, failureCallback);
+
 		
-		function successCallback(result){
+
+		}
+		
+			function getNumberAsArray(num) {
+				return new Array(num);
+			}
+		
+		function successCallback(result) {
 			console.log(result.data.result);
 			console.log(result.data.result.videos);
-			$scope.drinks=result.data.result;
-			$scope.videos=result.data.result.videos;
 		
+			$scope.drinks = result.data.result;
+			$scope.videos = result.data.result.videos;
+			console.log("amount",result.data.totalResult/12)
+			var number =parseInt(result.data.totalResult/12)
+			console.log("amount",number)
+			$scope.amount =getNumberAsArray(number)
+
+			
 		}
-		function failureCallback(result){
-			console.log(result);
-		}	
+		
+		
+		function failureCallback(result) {
+				console.log(result);
+			
 		};
+		
+	
+		
+		$scope.next = function(skip) {
+			console.log(skip)
+			skip = skip * 12
+			$scope.getDrinks(skip)
 
+		}
 	}
-
-
-
 
 })();
