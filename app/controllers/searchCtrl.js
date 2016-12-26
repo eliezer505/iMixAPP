1830d04b275afdd16113c788990f5dbe4a962f34
arama <aramainfo@gmail.com>
2016-12-26 18:17:55 +0200

    CLEAN

--- app/controllers/searchCtrl.js
+++ app/controllers/searchCtrl.js
@@ -4,54 +4,77 @@
 
 	//?apiKey=5178650f2fb747a1a4c48cf9b86bd154//
 
-	function searchCtrl($scope, $http) {
+	function searchCtrl($scope, $http, $timeout) {
 		var API_KEY = "?apiKey=5178650f2fb747a1a4c48cf9b86bd154";
 		var URL = "http://addb.absolutdrinks.com/drinks/";
 		var QUERY = "";
 		$scope.searchDrinks = "";
 		$scope.nextp = 0
 		$scope.title = "search ctrl";
+
+		function initData() {
+
+			
+			var promise = $http.jsonp("http://addb.absolutdrinks.com/quickSearch/drinks/a?apiKey=5178650f2fb747a1a4c48cf9b86bd154&pageSize=100&callback=JSON_CALLBACK");
+			promise.success(function(data) {
+          	availableTags=data.result;
+			 availableTags.map(function(item){
+			 		console.log(item.name);
+			 	return item.name;
+			
+			 
+			 });
+			 	
+				$("#tags").autocomplete({
+					source :  availableTags.map(function(item){
+			 		console.log(item.name);
+			 		
+			 	return item.name;
+			
+			 
+			 })
+				});
+
+			});
+		}
+
+		initData();
+
 		$scope.getDrinks = function(nextp) {
 			var promise = $http.jsonp("http://addb.absolutdrinks.com/quickSearch/drinks/" + $scope.searchDrinks + "/?apiKey=5178650f2fb747a1a4c48cf9b86bd154&start=" + nextp + "&pageSize=12&callback=JSON_CALLBACK");
 			promise.success(function(data) {
 				$scope.total = data.totalResult;
 				console.log("this is my data", data);
 				console.log("this is my data", data.result.videos);
-  
+
 			});
-			;
+
 			promise.then(successCallback, failureCallback);
 
-		
+		};
 
+		function getNumberAsArray(num) {
+			return new Array(num);
 		}
-		
-			function getNumberAsArray(num) {
-				return new Array(num);
-			}
-		
+
 		function successCallback(result) {
 			console.log(result.data.result);
 			console.log(result.data.result.videos);
-		
+
 			$scope.drinks = result.data.result;
 			$scope.videos = result.data.result.videos;
-			console.log("amount",result.data.totalResult/12)
-			var number =parseInt(result.data.totalResult/12)
-			console.log("amount",number)
-			$scope.amount =getNumberAsArray(number)
+			console.log("amount", result.data.totalResult / 12)
+			var number = parseInt(result.data.totalResult / 12)
+			console.log("amount", number)
+			$scope.amount = getNumberAsArray(number)
 
-			
 		}
-		
-		
+
 		function failureCallback(result) {
-				console.log(result);
-			
+			console.log(result);
+
 		};
-		
-	
-		
+
 		$scope.next = function(skip) {
 			console.log(skip)
 			skip = skip * 12
